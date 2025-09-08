410bb740a6d690650985c7fab2b6091f74af14f4
Pratik.Dutta <pratik.dutta@avalara.com>
2025-09-08 16:06:39 +0530

    structure media project with modules

---
+++ media-project-structured/src/main.rs
@@ -0,0 +1,37 @@
+mod content;
+
+use content::catalog::Catalog;
+use content::media::Media;
+
+fn main() {
+    let audiobook = Media::Audiobook {
+        title: String::from("An Audiobook"),
+    };
+    let good_movie = Media::Movie {
+        title: String::from("Good Movie"),
+        director: String::from("Good Director"),
+    };
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author"),
+    };
+    let podcast = Media::Podcast(10);
+    let placeholder = Media::Placeholder;
+
+    // println!("{}", audiobook.description());
+    // println!("{}", good_movie.description());
+    // println!("{}", bad_book.description());
+
+    let mut catalog = Catalog::new();
+
+    catalog.add(audiobook);
+    catalog.add(good_movie);
+    catalog.add(bad_book);
+    catalog.add(podcast);
+    catalog.add(placeholder);
+
+    let item = catalog.get_by_index(40);
+    let placeholder = Media::Placeholder;
+
+    println!("{:#?}", item.unwrap_or(&placeholder));
+}
