58884ce39ff6aceced5a631f88f97ddd5c3c940f
Pratik.Dutta <pratik.dutta@avalara.com>
2025-09-25 19:48:20 +0530

    add generic trait 3

---
+++ Generics/generics_3/src/main.rs
@@ -0,0 +1,18 @@
+use num_traits::ToPrimitive;
+
+// First version - we can pass in BOTH f32 or f64
+// Second - we can pass in any type of numbers
+
+fn solve<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
+
+fn main() {
+    let a: u8 = 3;
+    let b: f64 = 4.0;
+
+    println!("{}", solve(a, b));
+}
